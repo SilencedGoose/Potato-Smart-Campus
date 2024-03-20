0092a8ebcf9414d72b169170b43f7e51d71cc9b5
Solaris <fantailfishy@gmail.com>
2024-03-20 02:13:48 +0000

    x

--- potato-main/lib/program/sensor_node.ex
+++ potato-main/lib/program/sensor_node.ex
@@ -23,7 +23,10 @@ defmodule SensorNode do
 
   def read_measurement() do
     m = %{temperature: nil, humidity: nil, light: nil, noise: nil, motion: nil, co2: nil, sensor_failure: false}
-    m = case ElixirALE.I2C.start_link("i2c-1", 0x45) do
+    result = ElixirALE.I2C.start_link("i2c-1", 0x45)
+    IO.inspect(result)
+    # m = case ElixirALE.I2C.start_link("i2c-1", 0x45) do
+    m = case result do
       {:ok, i2c_pid} ->
         case SHT3x.single_shot_result(i2c_pid, :high, true) do
         [{:ok, temp}, {:ok, humidity}] -> Map.merge(m, %{humidity: Float.ceil(humidity, 1), temperature: Float.ceil(temp, 1)})
