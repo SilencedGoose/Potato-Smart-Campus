305ee369820530bf5dd74da1b2b004cab67198a9
AnimatorOfSouls <fantailfishy@gmail.com>
2024-04-10 07:53:06 +0100

    Fixed comments to actually be comments

--- potato-main/lib/program/server.ex
+++ potato-main/lib/program/server.ex
@@ -8,51 +8,51 @@ defmodule Server do
 
   def init() do
     # Our node descriptor.
-    nd = %{                                                                                                 //MN
-      hardware: :computer,                                                                                  //MN
-      type: :server,                                                                                        //MN
-      name: "server",                                                                                       //MN
-      uuid: ?a..?z |> Enum.shuffle() |> to_string                                                           //MN
-    }                                                                                                       //MN
+    nd = %{                                                                                                 #MN
+      hardware: :computer,                                                                                  #MN
+      type: :server,                                                                                        #MN
+      name: "server",                                                                                       #MN
+      uuid: ?a..?z |> Enum.shuffle() |> to_string                                                           #MN
+    }                                                                                                       #MN
 
-    Potato.Network.Meta.set_local_nd(nd)                                                                    //MN
+    Potato.Network.Meta.set_local_nd(nd)                                                                    #MN
   end
 
   defdag read_measurement(src, snk) do
-    src                                                                                                     //CO
-    ~> map(fn _ -> SensorNode.read_measurement() end)                                                       //SN
-  ~> snk                                                                                                    //CO
+    src                                                                                                     #CO
+    ~> map(fn _ -> SensorNode.read_measurement() end)                                                       #SN
+  ~> snk                                                                                                    #CO
   end
 
   defdag stream_measurements(src, snk, measurement_sink) do
-    src                                                                                                     //CO
-    ~> filter(fn event ->                                                                                   //CO
-      Kernel.match?({:join, _}, event)                                                                      //CO
-    end)                                                                                                    //CO
-    ~> map(fn {:join, device} ->                                                                            //CO
-      IO.inspect device, label: "device"                                                                    //CO
-        p =                                                                                                 //CO
-          program do                                                                                        //CO
-          deploy(read_measurement, src: Creek.Source.range(1, :inifinity, 0, 1000), snk: measurement_sink)  //CO
-          end                                                                                               //CO
+    src                                                                                                     #CO
+    ~> filter(fn event ->                                                                                   #CO
+      Kernel.match?({:join, _}, event)                                                                      #CO
+    end)                                                                                                    #CO
+    ~> map(fn {:join, device} ->                                                                            #CO
+      IO.inspect device, label: "device"                                                                    #CO
+        p =                                                                                                 #CO
+          program do                                                                                        #CO
+          deploy(read_measurement, src: Creek.Source.range(1, :inifinity, 0, 1000), snk: measurement_sink)  #CO
+          end                                                                                               #CO
 
-        Subject.next(device.deploy, p)                                                                      //CO
-    end)                                                                                                    //CO
-    ~> snk                                                                                                  //CO
+        Subject.next(device.deploy, p)                                                                      #CO
+    end)                                                                                                    #CO
+    ~> snk                                                                                                  #CO
   end
 
   defdag upload_measurement(src, snk) do
-    src                                                                                                     //CO
-    ~> map(fn v -> Repo.insert(%Measurement{temperature: v.temperature, humidity: v.humidity, light: v.light, motion: v.motion, noise: v.noise, co2: v.co2}) end)                                                               //DI
-    ~> snk                                                                                                  //CO
+    src                                                                                                     #CO
+    ~> map(fn v -> Repo.insert(%Measurement{temperature: v.temperature, humidity: v.humidity, light: v.light, motion: v.motion, noise: v.noise, co2: v.co2}) end)                                                               #DI
+    ~> snk                                                                                                  #CO
   end
 
   def run() do
-    init()                                                                                                  //CO
-    kill_switch = Creek.Sink.ignore(nil)                                                                    //CO
-    measurements = Creek.Source.gatherer()                                                                  //CO
-    deploy(stream_measurements, src: Net.network(), snk: kill_switch, measurement_sink: measurements)       //CO
-    deploy(upload_measurement, src: measurements, snk: kill_switch)                                         //CO
-    nil                                                                                                     //CO
+    init()                                                                                                  #CO
+    kill_switch = Creek.Sink.ignore(nil)                                                                    #CO
+    measurements = Creek.Source.gatherer()                                                                  #CO
+    deploy(stream_measurements, src: Net.network(), snk: kill_switch, measurement_sink: measurements)       #CO
+    deploy(upload_measurement, src: measurements, snk: kill_switch)                                         #CO
+    nil                                                                                                     #CO
   end
 end
--- smart_campus/lib/smart_campus_web/live/smart_campus_live.ex
+++ smart_campus/lib/smart_campus_web/live/smart_campus_live.ex
@@ -5,13 +5,13 @@ defmodule SmartCampusWeb.SmartCampusLive do
   alias SmartCampus.Measurement
 
   def mount(_params, _session, socket) do
-    if connected?(socket), do: Process.send_after(self(), :update_measurement, 1000)            //WI
+    if connected?(socket), do: Process.send_after(self(), :update_measurement, 1000)            #WI
     # SmartCampusWeb.Endpoint.subscribe("new_measurement")
-    IO.inspect(self())                                                                          //WI
-    {:ok, assign(socket, temperature: 0, humidity: 0, noise: 0, light: 0, motion: "Active", co2: 0, datetime: "17 Oct 2023 12:00:00")}                                                                            //WI
+    IO.inspect(self())                                                                          #WI
+    {:ok, assign(socket, temperature: 0, humidity: 0, noise: 0, light: 0, motion: "Active", co2: 0, datetime: "17 Oct 2023 12:00:00")}                                                                            #WI
   end
 
-  def render(assigns) do                                                                        //WI "lines: 11"
+  def render(assigns) do                                                                        #WI "lines: 11"
     ~H"""
     <div id="outputs">
       <p><b>Temperature:</b> <%= @temperature %></p>
@@ -26,14 +26,14 @@ defmodule SmartCampusWeb.SmartCampusLive do
   end
 
   def handle_info(:update_measurement, socket) do
-    Process.send_after(self(), :update_measurement, 5000)                                       //WI
-    measurement = Repo.one(from m in Measurement, order_by: [desc: m.inserted_at], limit: 1)    //DI
-      |> Map.from_struct                                                                        //WI
-      |> Enum.filter(fn {_, v} -> v != nil end)                                                 //WI
-      |> Enum.into(%{})                                                                         //WI
-    measurement = Map.put(measurement, :datetime, measurement.inserted_at)                      //WI
-    measurement = Map.drop(measurement, [:__meta__, :updated_at, :id, :inserted_at])            //WI
+    Process.send_after(self(), :update_measurement, 5000)                                       #WI
+    measurement = Repo.one(from m in Measurement, order_by: [desc: m.inserted_at], limit: 1)    #DI
+      |> Map.from_struct                                                                        #WI
+      |> Enum.filter(fn {_, v} -> v != nil end)                                                 #WI
+      |> Enum.into(%{})                                                                         #WI
+    measurement = Map.put(measurement, :datetime, measurement.inserted_at)                      #WI
+    measurement = Map.drop(measurement, [:__meta__, :updated_at, :id, :inserted_at])            #WI
 
-    {:noreply, assign(socket, measurement)}                                                     //WI
+    {:noreply, assign(socket, measurement)}                                                     #WI
   end
 end
